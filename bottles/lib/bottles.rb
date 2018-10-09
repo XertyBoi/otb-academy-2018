abdfeb308ed3056045c009233862a8c9ddb5b748
Harrison Coulter <harrison@Harrisons-MacBook-Pro.local>
2018-10-09 14:17:43 +0100

    refractored code

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,49 +8,66 @@ class Bottles
   end
 
   def verse(number)
+
+    bottle_number = BottleNumber.new(number)
+
     <<~VERSE
-      #{ quantity(number).capitalize } #{ container(number) } of beer on the wall, #{ quantity(number) } #{ container(number) } of beer.
-      #{ action(number) }, #{ quantity(successor(number)) } #{ container(successor(number)) } of beer on the wall.
+      #{ bottle_number.quantity_container.capitalize} of beer on the wall, #{bottle_number.quantity_container} of beer.
+      #{ bottle_number.action }, #{ bottle_number.successor.quantity_container} of beer on the wall.
       VERSE
   end
 
-  def container(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
+  private
+  class BottleNumber
 
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
-  end
+    attr_reader :number
 
-  def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
+    def initialize(number)
+      @number = number
     end
-  end
 
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{ pronoun(number) } down and pass it around"
+    def quantity_container
+      "#{quantity} #{container}"
     end
-  end
 
-  def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
+    def container
+      if number == 1
+        "bottle"
+      else
+        "bottles"
+      end
+    end
+
+    def pronoun
+      if number == 1
+        "it"
+      else
+        "one"
+      end
+    end
+
+    def quantity
+      if number == 0
+        "no more"
+      else
+        number.to_s
+      end
+    end
+
+    def action
+      if number == 0
+        "Go to the store and buy some more"
+      else
+        "Take #{ pronoun } down and pass it around"
+      end
+    end
+
+    def successor
+      if number == 0
+        BottleNumber.new(99)
+      else
+        BottleNumber.new(number - 1)
+      end
     end
   end
 end
