3aceac06c2832251b5c335bbb50bbcbd3dc6b937
Harrison Coulter <harrison@Harrisons-MacBook-Pro.local>
2018-10-09 11:10:27 +0100

    refractored bottle test

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -7,28 +7,56 @@ class Bottles
     start.downto(finish).map { |number| verse(number) }.join("\n")
   end
 
+
   def verse(number)
-    case number
-    when 0
       <<~VERSE
-        No more bottles of beer on the wall, no more bottles of beer.
-        Go to the store and buy some more, 99 bottles of beer on the wall.
+        #{quantity_of_container(number).capitalize} of beer on the wall, #{quantity_of_container number} of beer.
+        #{action number}, #{quantity_of_container number-1} of beer on the wall.
         VERSE
-    when 1
-      <<~VERSE
-        1 bottle of beer on the wall, 1 bottle of beer.
-        Take it down and pass it around, no more bottles of beer on the wall.
-        VERSE
-    when 2
-      <<~VERSE
-        2 bottles of beer on the wall, 2 bottles of beer.
-        Take one down and pass it around, 1 bottle of beer on the wall.
-        VERSE
+  end
+
+  private
+  def quantity_of_container(n)
+    "#{quantity n} #{container n}"
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
     else
-      <<~VERSE
-        #{number} bottles of beer on the wall, #{number} bottles of beer.
-        Take one down and pass it around, #{number - 1} bottles of beer on the wall.
-        VERSE
+      "Take #{pronoun number} down and pass it around"
+    end
+  end
+
+  def pronoun(n)
+    if n == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def quantity(n)
+    if n == 0
+      "no more"
+    else
+      minus_converter(n).to_s
+    end
+  end
+
+  def minus_converter(n)
+    if n < 0
+      99
+    else
+      n
+    end
+  end
+
+  def container(n)
+    if n == 1
+      "bottle"
+    else
+      "bottles"
     end
   end
 end
